e3a526d6ad7aa25d323dfaa704a5c7bb71e55647
jeevanandham-381 <jeevanandham381@gmail.com>
2023-12-31 20:21:51 +0530

    interface and imple lib added.

---
+++ Model/HospitalBO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model;
+
+public class HospitalBO
+{
+    public int Id { get; set; }
+    public int DistrictId { get; set; }
+    public int PatientId { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string ZipCode { get; set; }
+    public string Fax { get; set; }
+    public int StatusLid { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+
+}
---
+++ Model/PatientBO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model;
+
+public class PatientBO
+{
+    public int Id { get; set; }
+    public int HospitalId { get; set; }
+    public string Name { get; set; }
+    public string FatherName { get; set; }
+    public string Phone1 { get; set; }
+    public string Phone2 { get; set; }
+    public string Email { get; set; }
+    public string AdharNumber { get; set; }
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+}
